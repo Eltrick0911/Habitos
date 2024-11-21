d9cbcf9ebe84e6826d3ee9da9256e85c3dbcea27
Eltrick0911 <799patrick@gmail.com>
2024-11-20 22:24:22 -0600

    Creacion del Home

---
+++ Public/Home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>GOOD HABITS</title>
+    <link rel="stylesheet" href="../Public/ccs/Inicio.css" class="logo">
+
+</head>
+
+<body>
+
+    <img src="../Src/Img/Logo3.PNG" alt="Imagen de GOOD HABITS" class="logo">
+
+    <h1>Bienvenido a GOOD HABITS</h1>
+    <p>GOOD HABITS es una aplicación que te ayuda a construir hábitos saludables y alcanzar tus objetivos.
+        Organiza tu vida, aumenta tu productividad y mejora tu bienestar con nuestro sencillo sistema de
+        seguimiento de hábitos. ¡Comienza a construir una mejor versión de ti mismo hoy!</p>
+
+    <button onclick="window.location.href='../Src/Routes/views/register.php'">Registro</button>
+    <button onclick="window.location.href='../Src/Routes/views/login.php'">Inicio de Sesión</button>
+
+</body>
+
+</html>
