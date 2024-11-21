2c26be212136b9bddf4bd65696da1ecec795a1de
Eltrick0911 <799patrick@gmail.com>
2024-11-20 22:37:34 -0600

    Modificacion Css

--- Public/Home.php
+++ Public/Home.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
-<html>
-
-<head>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>GOOD HABITS</title>
-    <link rel="stylesheet" href="../Public/ccs/Inicio.css" class="logo">
-
-</head>
-
-<body>
-
-    <img src="../Src/Img/Logo3.PNG" alt="Imagen de GOOD HABITS" class="logo">
-
-    <h1>Bienvenido a GOOD HABITS</h1>
-    <p>GOOD HABITS es una aplicación que te ayuda a construir hábitos saludables y alcanzar tus objetivos.
-        Organiza tu vida, aumenta tu productividad y mejora tu bienestar con nuestro sencillo sistema de
-        seguimiento de hábitos. ¡Comienza a construir una mejor versión de ti mismo hoy!</p>
-
-    <button onclick="window.location.href='../Src/Routes/views/register.php'">Registro</button>
-    <button onclick="window.location.href='../Src/Routes/views/login.php'">Inicio de Sesión</button>
-
-</body>
-
+    <link rel="stylesheet" href="./ccs/Inicio.css" />
+  </head>
+  <body>
+    <header>
+      <img src="../Src/Img/Logo2.png" alt="Logo" />
+      <nav>
+        <button > <a href="../Src/Routes/views/login.php">Iniciar Sesión</a></button>
+        <button ><a href="../Src/Routes/views/register.php">Registrarse</a></button>
+      </nav>
+    </header>
+    <main>
+      <h1>Bienvenido a GOOD HABITS</h1>
+      <div class="info">
+        <p>
+          GOOD HABITS es una aplicación que te ayuda a construir hábitos
+          saludables y alcanzar tus objetivos. Organiza tu vida, aumenta tu
+          productividad y mejora tu bienestar con nuestro sencillo sistema de
+          seguimiento de hábitos.
+        </p>
+        <p>¡Comienza a construir una mejor versión de ti mismo hoy!</p>
+      </div>
+    </main>
+  </body>
 </html>
