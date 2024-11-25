257c42dbfd1e8831583fa46818ba318e304e85ef
Eglopez-lp4 <egarcial@unah.hn>
2024-11-24 23:11:36 -0600

    prueva de conexion

--- api-rest/api/consultar_usuario.php
+++ api-rest/api/consultar_usuario.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 require_once("../includes/clases/clase_usuario.php");
-use App\db\connectionDB;
+
 
 //$pdo = $database->abrirConexion();
 $id = filter_input(INPUT_GET,"id_Usuario");
