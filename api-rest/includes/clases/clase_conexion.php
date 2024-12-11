dd7c0fab587ddc948a34e751b372e093857c0928
Eltrick0911 <799patrick@gmail.com>
2024-12-10 21:31:13 -0600

    Configuracion de variables de entorno en conexiones

--- api-rest/includes/clases/clase_conexion.php
+++ api-rest/includes/clases/clase_conexion.php
@@ -1,26 +1,43 @@
 <?php
+require_once __DIR__ . '/../../../vendor/autoload.php'; 
 
+use Dotenv\Dotenv;
 
 class clase_conexion {
-    protected $database_host = "localhost";
-    protected $database_name = "seguimientohabitos";
-    protected $database_user = "root";
-    protected $database_pass = "252411";
-    
-    public function __construct(){
-    }    
-    
-    //abre conexion
+    protected $database_host;
+    protected $database_name;
+    protected $database_user;
+    protected $database_pass;
+
+    public function __construct() {
+        // Cargar las variables de entorno
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../../');
+        $dotenv->load();
+
+        $this->database_host = $_ENV['IP'];
+        $this->database_name = $_ENV['DB'];
+        $this->database_user = $_ENV['USER'];
+        $this->database_pass = $_ENV['PASSWORD'];
+    }
+
+    // Abre conexión
     public function abrirConexion() {
         $opciones = array(
-                PDO::ATTR_PERSISTENT => true
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         );
         try {
-            return new PDO("mysql:host=".$this->database_host.";dbname=".$this->database_name, $this->database_user, $this->database_pass,$opciones);
+            return new PDO(
+                "mysql:host=".$this->database_host.";dbname=".$this->database_name.";charset=utf8",
+                $this->database_user,
+                $this->database_pass,
+                $opciones
+            );
         } catch (PDOException $e) {
-            exit('ERROR al conectarse a la base datos. \nDescripción del ERROR: '.$e);
+            exit('ERROR al conectarse a la base de datos. \nDescripción del ERROR: '.$e->getMessage());
         }
-    } 
+    }
 }
+?>
 
 
--- bd/conexion.php
+++ bd/conexion.php
@@ -1,18 +1,33 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
 class Conexion extends PDO
 {
-    protected $database_host = "localhost";
-    protected $database_name = "seguimientohabitos";
-    protected $database_user = "root";
-    protected $database_pass = "252411";
-    
+    protected $database_host;
+    protected $database_name;
+    protected $database_user;
+    protected $database_pass;
+
     public function __construct()
     {
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+        $dotenv->load();
+
+        $this->database_host = $_ENV['IP'];
+        $this->database_name = $_ENV['DB'];
+        $this->database_user = $_ENV['USER'];
+        $this->database_pass = $_ENV['PASSWORD'];
+
         try {
-            parent::__construct('mysql:host=' . $this->database_host . ';dbname=' . $this->database_name . ';charset=utf8', 
-                $this->database_user, $this->database_pass, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-            
-        } catch(PDOException $e) {
+            parent::__construct(
+                'mysql:host=' . $this->database_host . ';dbname=' . $this->database_name . ';charset=utf8',
+                $this->database_user,
+                $this->database_pass,
+                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+            );
+        } catch (PDOException $e) {
             echo 'Error: ' . $e->getMessage();
             exit;
         }
